260b11bd9a2f12924b81fa080d29df0fe10cf020
Illya <pivas47@gmail.com>
2020-03-13 01:45:21 +0200

    introducing adding experts into the array

--- turner.py
+++ turner.py
@@ -1,7 +1,7 @@
 import os
 
 # https://openpyxl.readthedocs.io/en/stable/
-from openpyxl import load_workbook
+from openpyxl import *
 
 # What is the current shift
 # https://stackoverflow.com/a/103081
@@ -18,7 +18,7 @@ def shiftPicker(currentHour):
 		'23': "night"
 	}.get(currentHour, "night")
 currentShift = shiftPicker (currentHour)
-print(currentShift)
+#print(currentShift)
 
 # reading the stings into variables
 STRINGS = open("STRINGS", "r")
@@ -33,9 +33,69 @@ D_MORNING = STRINGS.readline()
 D_EVENING = STRINGS.readline()
 STRINGS.close()
 
+# slicing out the last symbol
+DPT1_TAG = DPT1_TAG[0:len(DPT1_TAG) - 1]
+DPT2_TAG = DPT2_TAG[0:len(DPT2_TAG) - 1]
+EXPERT_TAG = EXPERT_TAG[0:len(EXPERT_TAG) - 1]
+K_NIGHT = K_NIGHT[0:len(K_NIGHT) - 1]
+K_MORNING = K_MORNING[0:len(K_MORNING) - 1]
+K_EVENING = K_EVENING[0:len(K_EVENING) - 1]
+D_NIGHT = D_NIGHT[0:len(D_NIGHT) - 1]
+D_MORNING = D_MORNING[0:len(D_MORNING) - 1]
+D_EVENING = D_EVENING[0:len(D_EVENING) - 1]
+
+
 # loading worksheets from workbooks
 dnXls = load_workbook('dn.xlsx').active
 khXls = load_workbook('kh.xlsx').active
+
+# Expert names to be inserted into JSON
+# We are filling this array only with those who will be on the shift
+experts = []
+experts.append("who who")
+
+# TODO switch 300 to purposly defined value
+colB = dnXls['B']
+
+# returns correct string of the time depending on the shift type
+def timePickerD(currentShift):
+	return {
+		'morning': D_MORNING,
+		'evening': D_EVENING,
+		'night': D_NIGHT
+	}.get(currentShift, "night")
+def timePickerK(currentShift):
+	return {
+		'morning': K_MORNING,
+		'evening': K_EVENING,
+		'night': K_NIGHT
+	}.get(currentShift, "night")
+
+# Constructing D experts
+for cell in colB:
+	if cell.value == EXPERT_TAG:
+
+		# converting cell object into cell row number (still string)
+		# <Cell u'Schedule'.
+		# https://developers.google.com/edu/python/strings
+		cell = str(cell)[19:len(str(cell)) - 1]
+		if timePickerD(currentShift) in (dnXls['D' + cell].value):
+			experts.append(dnXls['A' + cell].value)
+
+colB = khXls['B']
+# Constructing K experts
+for cell in colB:
+	if cell.value == EXPERT_TAG:
+
+		# converting cell object into cell row number (still string)
+		# <Cell u'Schedule'.
+		# https://developers.google.com/edu/python/strings
+		cell = str(cell)[19:len(str(cell)) - 1]
+		if timePickerK(currentShift) in (khXls['D' + cell].value):
+			experts.append(khXls['A' + cell].value)
+
+# prints experts on the current shift
+print (experts) #TODO TODO TODO TODO TODO TODO TODO TODO
 
 # removing them from the memory
 del dnXls
