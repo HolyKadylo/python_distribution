3f11dc154b471f05148baf578718fc304a4a921c
Illya <pivas47@gmail.com>
2020-03-12 21:52:39 +0200

    reading STRINGS

--- turner.py
+++ turner.py
@@ -6,3 +6,15 @@ import os
 stream = os.popen('date +%d.%m.%Y')
 output = stream.read()
 output
+
+# reading the stings into variables
+STRINGS = open("STRINGS", "r")
+DPT1_TAG = STRINGS.readline()
+DPT2_TAG = STRINGS.readline()
+EXPERT_TAG = STRINGS.readline()
+K_NIGHT = STRINGS.readline()
+K_MORNING = STRINGS.readline()
+K_EVENING = STRINGS.readline()
+D_NIGHT = STRINGS.readline()
+D_MORNING = STRINGS.readline()
+D_EVENING = STRINGS.readline()
